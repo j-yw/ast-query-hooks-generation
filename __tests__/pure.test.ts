0c30b1715bb760db9630c40ba6db1bf5dc9fc5c1
j-yw <32629001+j-yw@users.noreply.github.com>
2024-08-24 19:16:59 +0800

    chore: linting

--- __tests__/pure.test.ts
+++ __tests__/pure.test.ts
@@ -1,41 +1,28 @@
-import babelTraverse from '@babel/traverse';
-import { parse, ParserPlugin } from '@babel/parser';
-import generate from '@babel/generator';
-import * as t from '@babel/types';
+import generate from "@babel/generator";
+import * as t from "@babel/types";
 
 const expectCode = (ast) => {
-    expect(
-        generate(ast).code
-    ).toMatchSnapshot();
-}
+  expect(generate(ast).code).toMatchSnapshot();
+};
 
 const printCode = (ast) => {
-    console.log(
-        generate(ast).code
-    );
-}
+  console.log(generate(ast).code);
+};
 
-
-it('works', () => {
-
-    expectCode(t.file(t.program(
-        [
-            t.variableDeclaration(
-                'const',
-                [
-                    t.variableDeclarator(
-                        t.identifier('myCoolFunction'),
-                        t.arrowFunctionExpression(
-                            [
-                                t.identifier('some')
-                            ],
-                            t.blockStatement([])
-                        )
-                    )
-                ]
+it("works", () => {
+  expectCode(
+    t.file(
+      t.program([
+        t.variableDeclaration("const", [
+          t.variableDeclarator(
+            t.identifier("myCoolFunction"),
+            t.arrowFunctionExpression(
+              [t.identifier("some")],
+              t.blockStatement([])
             )
-        ]
-    )));
-
-
+          ),
+        ]),
+      ])
+    )
+  );
 });
--- __tests__/visitor.test.ts
+++ __tests__/visitor.test.ts
@@ -1,51 +1,39 @@
-import babelTraverse from '@babel/traverse';
-import { parse, ParserPlugin } from '@babel/parser';
-import generate from '@babel/generator';
-import * as t from '@babel/types';
+import babelTraverse from "@babel/traverse";
+import { parse, ParserPlugin } from "@babel/parser";
+import generate from "@babel/generator";
 
 const expectCode = (ast) => {
-    expect(
-        generate(ast).code
-    ).toMatchSnapshot();
-}
+  expect(generate(ast).code).toMatchSnapshot();
+};
 
 const printCode = (ast) => {
-    console.log(
-        generate(ast).code
-    );
-}
+  console.log(generate(ast).code);
+};
 
-
-it('works', () => {
-
-    const code = `
+it("works", () => {
+  const code = `
 export interface MyInterface {
     a: string;
     b: number;
 }
 `;
 
-    const plugins: ParserPlugin[] = [
-        'typescript',
-    ];
+  const plugins: ParserPlugin[] = ["typescript"];
 
-    const ast = parse(code, {
-        sourceType: 'module',
-        plugins
-    });
+  const ast = parse(code, {
+    sourceType: "module",
+    plugins,
+  });
 
-    babelTraverse(ast, {
-        TSTypeAliasDeclaration(path) {
-            console.log(path.node.id.name, path.parentPath.node);
-        },
-        TSInterfaceDeclaration(path) {
-            path.node.id.name = 'MyOtherInterface'
-            console.log(path.node.id.name, path.parentPath.node);
-        }
-    });
+  babelTraverse(ast, {
+    TSTypeAliasDeclaration(path) {
+      console.log(path.node.id.name, path.parentPath.node);
+    },
+    TSInterfaceDeclaration(path) {
+      path.node.id.name = "MyOtherInterface";
+      console.log(path.node.id.name, path.parentPath.node);
+    },
+  });
 
-
-    expectCode(ast);
-
-
+  expectCode(ast);
 });
